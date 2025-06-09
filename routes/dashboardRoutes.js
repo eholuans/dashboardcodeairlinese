0a1e276ada5152014b899468ecbb3883fd6c2815
eholuans <web3luan@gmail.com>
2025-06-09 13:29:43 -0300

    Update dashboardRoutes.js

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -2,13 +2,10 @@ const express = require('express');
 const router = express.Router();
 const Voo = require('../models/Voo');
 
-// Rota para a página principal do dashboard
 router.get('/', async (req, res) => {
   try {
-    // Obter estatísticas para o dashboard
     const estatisticas = await Voo.getEstatisticas();
     
-    // Obter alguns voos recentes para exibir no dashboard
     const voosRecentes = await Voo.findAll({ limit: 4 });
     
     res.render('dashboard', {
